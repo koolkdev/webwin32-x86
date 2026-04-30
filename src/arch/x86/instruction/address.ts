23f76f95b3196b99619be6864e8c8dc379642b1b
koolkdev <koolkdev@gmail.com>
2026-05-01 00:49:23 +0300

    Remove legacy x86 decoder sources

--- src/arch/x86/instruction/address.ts
+++
@@ -1,7 +0,0 @@
-import { u32 } from "../../../core/state/cpu-state.js";
-import type { DecodedInstruction } from "./types.js";
-
-export function instructionEnd(instruction: DecodedInstruction): number {
-  return u32(instruction.address + instruction.length);
-}
-
